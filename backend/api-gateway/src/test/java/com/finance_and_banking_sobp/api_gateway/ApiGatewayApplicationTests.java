24f5ac20443a8fc743a2c84ad809e372ab709d66
Aaryan jadon <aryanjadon13@gmail.com>
2026-04-05 15:16:23 +0530

    added api gateway service with routing to user service

---
+++ backend/api-gateway/src/test/java/com/finance_and_banking_sobp/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance_and_banking_sobp.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
